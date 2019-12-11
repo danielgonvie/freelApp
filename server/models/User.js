058e11dd45075e81c2ad9ea062ffabf84af90b1c
Daniel <daniel.gonvie@gmail.com>
2019-12-11 21:03:46 +0100

    Push

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
+const IMG_URL = /.*\.(gif|jpe?g|bmp|png)$/igm
 
 const userSchema = new Schema({
-  username: String,
-  password: String
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 2 },
+  picture: { type: String, default: 'https://i.stack.imgur.com/l60Hf.png' },
+  name: { type: String, required: true},
+  topArtists: []
 }, {
   timestamps: {
     createdAt: 'created_at',
